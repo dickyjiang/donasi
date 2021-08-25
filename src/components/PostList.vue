6e87a606a44cccb310d0090f7c51fc5e54c7a02d
Dicky Jiang <dickyjuwono@gmail.com>
2021-08-26 03:53:09 +0700

    lesson  79  min 8:00

---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+         <SinglePost :post="post" />
+      </div>
+      
+
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+
+export default {
+    props: ['posts'],
+    components: { SinglePost},
+    setup(props) {
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component Unmounted'))
+        onUpdated (() => console.log('Updated') )
+
+    }
+    
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,77 @@
 <template>
   <div class="Home">
-    <div v-if="projects.length">
+      <h1>HOME</h1>
+      <PostList v-if="showPosts" :posts="posts" />
+      <button @click="showPosts = !showPosts">toggle posts</button>
+      <button @click="posts.pop()">Delete a Post</button>
+    <!-- <div v-if="projects.length">
         <div v-for="project in projects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
         </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 import SingleProject from '../components/SingleProject.vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
     name: 'Home',
-    components: { SingleProject},
-    data() {
-        return {
-            projects:[]
-        }
-    },
-    mounted() {
-        fetch('http://localhost:3000/projects')
-        .then(res => res.json())
-        .then(data => this.projects = data )
-        .catch(err => console.log(err.message))
-    },
-    methods : {
-        handleDelete(id) {
-            this.projects = this.projects.filter((project) => {
-                return project.id !== id
-            })
+    components: { SingleProject, PostList,},
+    
+    setup() {
 
-        },
-        handleComplete(id) {
-            let p = this.projects.find(project => {
-                return project.id == id 
-            })
-            p.complete = !p.complete
+    const posts = ref([])
+    const showPosts = ref(true)
+    const error = ref(null)
+
+    const load = async () => {
+        try{
+            let data = await fetch('http://localhost:3000/projects')
+            if (!data.ok) {
+                throw Error('no data available')
+            }
+            
+        }
+        catch (err) {
+            error.value = err.message
+            console.log(error.value);
 
         }
     }
+        load()
 
+    return { posts, showPosts,  }
 
+    },
+    // data() {
+    //     return {
+    //         projects:[]
+    //     }
+    // },
+    // mounted() {
+    //     fetch('http://localhost:3000/projects')
+    //     .then(res => res.json())
+    //     .then(data => this.projects = data )
+    //     .catch(err => console.log(err.message))
+    // },
+    // methods : {
+    //     handleDelete(id) {
+    //         this.projects = this.projects.filter((project) => {
+    //             return project.id !== id
+    //         })
+
+    //     },
+    //     handleComplete(id) {
+    //         let p = this.projects.find(project => {
+    //             return project.id == id 
+    //         })
+    //         p.complete = !p.complete
+
+    //     }
+    // }
 
 }
 </script>
