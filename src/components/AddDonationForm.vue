a479ee9704aaa08d2ca66e9ef31eb475574cd6be
Dicky Jiang <dickyjuwono@gmail.com>
2021-08-30 12:14:54 +0700

    format doc

--- src/components/AddDonationForm.vue
+++ src/components/AddDonationForm.vue
@@ -1,26 +1,75 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <div class=" border-4 border-gray-300 pt-4 pb-4 px-4 bg-gray-400">
-      <div class="flex flex-col sm:flex-row sm:flex justify-around my-3 ">
+    <div class="border-4 border-gray-300 pt-4 pb-4 px-4 bg-gray-400">
+      <div class="flex flex-col sm:flex-row sm:flex justify-around my-3">
         <div
-          class="mt-2 md:mt-0 w-1/3 mx-2 bg-yellow-500 border-2  shadow-sm border-gray-600 text-xl font-bold focus:ring-2 focus:ring-yellow-700 py-2 px-3"
+          class="
+            mt-2
+            md:mt-0
+            w-1/3
+            mx-2
+            bg-yellow-500
+            border-2
+            shadow-sm
+            border-gray-600
+            text-xl
+            font-bold
+            focus:ring-2 focus:ring-yellow-700
+            py-2
+            px-3
+          "
         >
           10.000
         </div>
         <div
-          class="py-2 px-3 mt-2 md:mt-0 w-1/3 mx-2 bg-yellow-500 border-2  shadow-sm border-gray-600 text-xl font-bold focus:ring-2 focus:ring-yellow-700 "
+          class="
+            py-2
+            px-3
+            mt-2
+            md:mt-0
+            w-1/3
+            mx-2
+            bg-yellow-500
+            border-2
+            shadow-sm
+            border-gray-600
+            text-xl
+            font-bold
+            focus:ring-2 focus:ring-yellow-700
+          "
         >
           50.000
         </div>
         <div
-          class="py-2 px-3 mt-2 md:mt-0 w-1/3 mx-2 bg-yellow-500 border-2  shadow-sm border-gray-600 text-xl font-bold focus:ring-2 focus:ring-yellow-700 "
+          class="
+            py-2
+            px-3
+            mt-2
+            md:mt-0
+            w-1/3
+            mx-2
+            bg-yellow-500
+            border-2
+            shadow-sm
+            border-gray-600
+            text-xl
+            font-bold
+            focus:ring-2 focus:ring-yellow-700
+          "
         >
           100.000
         </div>
       </div>
 
       <input
-        class="p-2 border-2 border-gray-200 text-xl font-bold mt-2 focus:ring-2 focus:ring-yellow-700 "
+        class="
+          p-2
+          border-2 border-gray-200
+          text-xl
+          font-bold
+          mt-2
+          focus:ring-2 focus:ring-yellow-700
+        "
         type="number"
         required
         placeholder="0 IDR"
@@ -30,7 +79,17 @@
 
     <div class="mt-8 border-4 border-gray-300 p-4">
       <input
-        class=" border-2 border-gray-200 text-md font-bold mt-4 focus: outline-none focus:ring-2 focus:ring-yellow-700 py-2 px-3"
+        class="
+          border-2 border-gray-200
+          text-md
+          font-bold
+          mt-4
+          focus:
+          outline-none
+          focus:ring-2 focus:ring-yellow-700
+          py-2
+          px-3
+        "
         type="text"
         required
         placeholder="Nama"
@@ -38,7 +97,17 @@
       />
 
       <input
-        class=" border-2 border-gray-200 text-md font-bold mt-4 focus: outline-none focus:ring-2 focus:ring-yellow-700 py-2 px-3"
+        class="
+          border-2 border-gray-200
+          text-md
+          font-bold
+          mt-4
+          focus:
+          outline-none
+          focus:ring-2 focus:ring-yellow-700
+          py-2
+          px-3
+        "
         type="email"
         required
         placeholder="Email"
@@ -47,7 +116,17 @@
       <div class="mt-6">
         <p class="text-left pl-3 text-md font-bold mt-3 text-gray-400">Pesan</p>
         <textarea
-          class="border-4 border-gray-300 text-xl font-bold mt-3 focus: outline-none focus:ring-2 focus:ring-yellow-700 py-2 px-3"
+          class="
+            border-4 border-gray-300
+            text-xl
+            font-bold
+            mt-3
+            focus:
+            outline-none
+            focus:ring-2 focus:ring-yellow-700
+            py-2
+            px-3
+          "
           name=""
           id=""
           cols="30"
@@ -59,7 +138,16 @@
     <!-- /pilih payment gateway -->
 
     <div
-      class="flex flex-col md:flex-row md:items-center justify-around items-baseline mt-6 border-4 border-gray-300 p-2"
+      class="
+        flex flex-col
+        md:flex-row
+        md:items-center
+        justify-around
+        items-baseline
+        mt-6
+        border-4 border-gray-300
+        p-2
+      "
     >
       <div class="p-2">
         <img class="h-6 mb-3" src="../assets/Logo_ovo_purple.svg" alt="ovo" />
@@ -77,7 +165,24 @@
 
     <div class="error">{{ error }}</div>
     <button
-      class="mt-12 w-full max-w-lg py-2 px-3 focus:outline-none focus:bg-yellow-800 rounded-none bg-yellow-600 text-lg font-semibold shadow-lg transform hover:-translate-y-1 ease-in-out duration-700"
+      class="
+        mt-12
+        w-full
+        max-w-lg
+        py-2
+        px-3
+        focus:outline-none
+        focus:bg-yellow-800
+        rounded-none
+        bg-yellow-600
+        text-lg
+        font-semibold
+        shadow-lg
+        transform
+        hover:-translate-y-1
+        ease-in-out
+        duration-700
+      "
     >
       Kirim
     </button>
