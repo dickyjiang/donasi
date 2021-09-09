4848700522232bb30e2c68bcb79a722d7b412342
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-09 08:51:08 +0700

    betulisn styling page display

--- src/views/Display.vue
+++ src/views/Display.vue
@@ -2,25 +2,25 @@
 <div class="min-h-screen bg-turkis p-4">
     <div class="flex flex-col h-screen justify-center items-center  font-semibold max-w-lg mx-auto ">
 
-      <div class="border-2 border-green-300 py-8 px-8 rounded-3xl text-white  shadow-xl w-full">
+      <div class=" py-8 px-8 rounded-3xl text-white w-full">
 
           <div class="flex justify-between gap-10 items-stretch">
-                <div class="flex-1 flex  items-end text-6xl">
+                <div class="flex-1 flex  items-end text-5xl">
                   <div class="text-2xl">IDR</div>
                   <div> {{ $route.query.rp }} </div>
                 </div>
                 
           </div>
 
-          <div class="font-serif font-semibold text-4xl my-8 text-center">
+          <div class="font-serif font-semibold text-4xl my-5 text-center">
               " {{ $route.query.pesan }} "
           </div>
 
           <div class="w-full text-right">
-                  <p class="text-md">
+                  <!-- <p class="text-md">
                     Pengirim 
-                  </p>
-                  <div class="text-4xl font-bold">
+                  </p> -->
+                  <div class="text-3xl font-bold">
                     {{ $route.query.kepada }}
                   </div>
                   </div>
@@ -34,7 +34,8 @@
 <script>
 export default {
   name: "display",
-  props: ["rp", "kepada"],
+  props: ["rp", "kepada", "pesan"],
+    
   data() {
     return {
       donations: [],
