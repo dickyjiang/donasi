ddc89c35796b3238ff3cbb1b4c01da24d0a18b04
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-12 19:16:07 +0700

    WIP

--- src/views/CreateProfile.vue
+++ src/views/CreateProfile.vue
@@ -1,45 +1,68 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <h3>Create Profile</h3>
-    <label>
-      Nama
-    </label>
-    <input class="appearance-none " type="text" required placeholder="Nama yang akan ditampilkan" v-model="profileName">
-    <textarea class="mt-12" placeholder="Mengenai anda ..." v-model="description"></textarea>
-    <div class="mt-2">
-      <label class=" inline-block mx-auto text-center bg-turkis text-white py-3 px-5 rounded">
-          <input class="hidden" type="file" @change="handleChange"> upload profile picture
-      </label>
-      <div> {{fileError}}</div>
-    </div>
-    <div>
-      <label>
-        Background Picture
-      </label>
-      <input type="file">
-    </div>
-    <button>Save Profile</button>
-  </form>
+  <div class="flex flex-col justify-between items-center mx-auto w-full max-w-md border-2 mt-8 " >
+      <form class="flex flex-col w-full items-center" @submit.prevent="handleSubmit">
+        <h2 class="text-2xl font-bold ">Create Profile</h2>
+        <div class="mt-8">
+          <label class="text-left">
+            Nama
+          </label>
+          <input class="" type="text" required placeholder="Nama yang akan ditampilkan" v-model="profileName">
+          <textarea class="mt-12" placeholder="Mengenai anda ..." v-model="description"></textarea>
+          <div class="mt-4">
+            <label  class="mx-auto text-center bg-turkis text-white py-3 px-5 rounded">
+                <input class="hidden" type="file" @change="handleChange"> upload profile picture
+            </label>
+            <div> {{fileError}}</div>
+          </div>
+          <button v-if="!isPending" class="mt-12 w-full rounded max-w-lg py-2 px-5 focus:outline-none focus:bg-yellow-600 bg-yellow-500 text-lg text-white font-semibold shadow-lg transform hover:-translate-y-1 ease-in-out duration-300 ">Save Profile</button>
+          <button v-else disabled class="mt-12 w-full rounded max-w-lg py-2 px-5 focus:outline-none focus:bg-yellow-600 bg-yellow-500 text-lg text-white font-semibold shadow-lg transform hover:-translate-y-1 ease-in-out duration-300 ">Uploading ...</button>
+        </div>
+      </form>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 import  useStorage  from '../composables/useStorage'
+import useCollection from '../composables/useCollection'
+import getUser from '../composables/getUser'
+import { timestamp } from '../firebase/config'
+
 
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage()
+    const { error, addDoc } = useCollection('profiles')
+    const { user } = getUser()
 
     const profileName = ref('')
     const description = ref('')
     const file = ref(null)
     const fileError = ref(null)
+    const isPending = ref(false)
 
     const handleSubmit = async () => {
       if (file.value){
+        isPending.value = true
         await uploadImage(file.value)
-        console.log('image uploaded, url: ',url.value)
-        console.log(profileName.value, description.value, file.value)
+        await addDoc ({
+          profileName: profileName.value,
+          description: description.value,
+          userId: user.value.uid,
+          userName: user.value.displayName,
+          avatarUrl: url.value,
+          filePath: filePath.value,
+          createdAt: timestamp()
+
+        })
+        isPending.value = false
+
+        if (!error.value) {
+          console.log('profile added')
+        }
+
+        // console.log('image uploaded, url: ',url.value)
+        // console.log(profileName.value, description.value, file.value)
       }
     }
 
@@ -61,7 +84,7 @@ export default {
 
     }
 
-    return { profileName, description, handleSubmit, handleChange, fileError}
+    return { profileName, description, handleSubmit, handleChange, fileError, isPending}
   }
 
 }
